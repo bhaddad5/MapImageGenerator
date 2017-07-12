b3625114bfac3087036845fd9326ac258129d7e2
bhaddad5 <bhaddad5@gmail.com>
2017-07-11 22:40:26 -0700

    Some basic rts stuff working.  Still not sure if I'll use it though...

---
+++ Assets/RTSTest/Scripts/TroopController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroopController : MonoBehaviour
+{
+	public float speed = 0.02f;
+
+	public void MoveTowardsDesiredPos(Vector3 pos, Vector3 rot)
+	{
+		if (transform.position.FromTo(pos).magnitude < speed)
+		{
+			transform.position = pos;
+			transform.eulerAngles = rot;
+		}
+		else
+		{
+			Vector3 newPos = transform.position + transform.position.FromTo(pos).normalized * speed;
+			newPos.y = 0;
+			transform.position = newPos;
+			transform.LookAt(pos);
+		}
+		
+	}
+}
---
+++ Assets/RTSTest/Scripts/UnitController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+	public int numTroops;
+	public GameObject troopPrefab;
+
+	private List<TroopController> troops = new List<TroopController>();
+
+	private int numLines = 3;
+	private float spacing = 0.2f;
+
+	void Start()
+	{
+		int i = 0;
+		while (i < numTroops)
+		{
+			troops.Add(Instantiate(troopPrefab).GetComponent<TroopController>());
+			i++;
+		}
+	}
+
+	private Vector3 GetDesiredPos(int index)
+	{
+		int numTroopsInLine = troops.Count / numLines;
+		int myLine = index / numTroopsInLine;
+		int myColumn = index % numTroopsInLine;
+
+		Vector3 offset = new Vector3((numTroopsInLine/2f) * spacing, 0, ((troops.Count%numTroopsInLine)/2f) * spacing);
+
+		Vector3 localOffset = new Vector3(myColumn * spacing - offset.x, 0, myLine * spacing - offset.z);
+
+		return transform.position - Quaternion.Euler(transform.eulerAngles) * localOffset;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		int i = 0;
+		foreach (TroopController troop in troops)
+		{
+			troop.MoveTowardsDesiredPos(GetDesiredPos(i), transform.eulerAngles);
+
+			i++;
+		}
+	}
+}
