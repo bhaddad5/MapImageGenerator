d07d751c650992dbe033449454de74bdf8578f31
bhaddad5 <bhaddad5@gmail.com>
2017-07-11 23:08:54 -0700

    Some basic pathing

--- Assets/RTSTest/Scripts/TroopController.cs
+++ Assets/RTSTest/Scripts/TroopController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TroopController : MonoBehaviour
 {
 	public float speed = 0.02f;
+
+	private float radius = 0.01f;
 
 	public void MoveTowardsDesiredPos(Vector3 pos, Vector3 rot)
 	{
@@ -15,11 +18,32 @@ public class TroopController : MonoBehaviour
 		}
 		else
 		{
-			Vector3 newPos = transform.position + transform.position.FromTo(pos).normalized * speed;
-			newPos.y = 0;
-			transform.position = newPos;
+			transform.position = TestGetNewPos(pos);
 			transform.LookAt(pos);
 		}
 		
+	}
+
+	private Vector3 TestGetNewPos(Vector3 targetPos)
+	{
+		Vector3 desiredDir = transform.position.FromTo(targetPos);
+		for (int i = 0; i < 8; i++)
+		{
+			Vector3 newDir = Quaternion.Euler(0, i * 45, 0) * desiredDir;
+			Vector3 newPos = GetNewPos(newDir);
+
+			Collider[] coll = Physics.OverlapSphere(newPos + new Vector3(0, 0.05f, 0), radius);
+			if (coll.Length == 0)
+			{
+				return newPos;
+			}
+				
+		}
+		return GetNewPos(desiredDir);
+	}
+
+	private Vector3 GetNewPos(Vector3 dir)
+	{
+		return transform.position + dir.normalized * speed;
 	}
 }
