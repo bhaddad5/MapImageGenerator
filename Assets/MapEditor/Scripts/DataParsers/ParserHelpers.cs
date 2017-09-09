334c2c49f9c45bf63ce0c691fd0ddc8e89a431ff
bhaddad5 <bhaddad5@gmail.com>
2017-09-09 15:07:28 -0700

    added parser helpers

---
+++ Assets/MapEditor/Scripts/DataParsers/ParserHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public static class ParserHelpers {
+
+	public static string ClearOutComments(string file)
+	{
+		return RemoveBetween(file, "//", "\n");
+	}
+
+	private static string RemoveBetween(string s, string begin, string end)
+	{
+		Regex regex = new Regex(string.Format("\\{0}.*?\\{1}", begin, end));
+		return regex.Replace(s, string.Empty);
+	}
+}
