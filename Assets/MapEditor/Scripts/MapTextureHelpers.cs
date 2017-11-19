608d8231dab1f051a528972a36635327946eda8c
bhaddad5 <bhaddad5@gmail.com>
2017-11-19 13:14:30 -0800

    "Wooo!  Fuck yeah!"  -Bert Haddad, 2017

--- Assets/MapEditor/Scripts/MapTextureHelpers.cs
+++ Assets/MapEditor/Scripts/MapTextureHelpers.cs
@@ -11,32 +11,19 @@ public class MapTextureHelpers
 		Map2D<Color> mapTex = new Map2D<Color>(Map.Width * tileSize, Map.Height * tileSize);
 		foreach (Int2 point in mapTex.GetMapPoints())
 		{
-			if(point.X == 70 && point.Y == 70)
-				Debug.Log("HIT!");
-
-			Int2 mapPoint = point / tileSize;
-			Vector2 mapFraction = new Vector2(((point.X - mapPoint.X * tileSize))/(float) tileSize, (point.Y - mapPoint.Y * tileSize) / (float)tileSize);
+			Int2 mapPoint = (point - new Int2(tileSize/2, tileSize/2)) / tileSize;
+			Vector2 mapFraction = new Vector2(((point.X - mapPoint.X * tileSize - tileSize / 2))/(float) tileSize, (point.Y - mapPoint.Y * tileSize - tileSize / 2) / (float)tileSize);
 
 			Color interp = (1 - mapFraction.x) *
-			               ((1 - mapFraction.y) * GetColor(point, mapPoint, Map)) +
-			               mapFraction.y * GetColor(point, mapPoint + new Int2(0, 1), Map) +
+			               ((1 - mapFraction.y) * GetColor(point, mapPoint, Map) +
+			               mapFraction.y * GetColor(point, mapPoint + new Int2(0, 1), Map)) +
 						   mapFraction.x *
-						   ((1 - mapFraction.y) * GetColor(point, mapPoint + new Int2(1, 0), Map)) +
-						   mapFraction.y * GetColor(point, mapPoint + new Int2(1, 1), Map);
+						   ((1 - mapFraction.y) * GetColor(point, mapPoint + new Int2(1, 0), Map) +
+						   mapFraction.y * GetColor(point, mapPoint + new Int2(1, 1), Map));
 
 			interp = interp / interp.a;
 
-			/*interpolatedValue = (1 - fractionX) *
-			                    ((1 - fractionY) * data[integerX, integerY] +
-			                     fractionY * data[integerX, integerY + 1]) +
-			                    fractionX *
-			                    ((1 - fractionY) * data[integerX + 1, integerY] +
-			                     fractionY * data[integerX + 1, integerY + 1]);*/
-
 			mapTex.Set(point, interp);
-
-			//Map2D<Color> colors = Map.Get(mapPoint).Terrain().GetTerrainTexture().Colors;
-			//mapTex.Set(point, colors.Get(new Int2(point.X, point.Y), true));
 		}
 		return mapTex;
 	}
@@ -48,19 +35,6 @@ public class MapTextureHelpers
 		Color res = Map.Get(mapPos).Terrain().GetTerrainTexture().Colors.Get(texPos, true);
 		return res;
 	}
-
-	/*private static Color GetAdjustedColor(Int2 myPos, Int2 centerPointToTestAgainst, int tileSize, Map2D<MapTileModel> Map)
-	{
-		if(!Map.PosInBounds(centerPointToTestAgainst))
-			return new Color(0, 0, 0, 0);
-
-		float dist = new Vector2(myPos.X, myPos.Y).FromTo(new Vector2(centerPointToTestAgainst.X * tileSize, centerPointToTestAgainst.Y * tileSize)).magnitude;
-		float colorStrength = Mathf.Max(0, (tileSize*2f - dist)/tileSize);
-		Color res = Map.Get(centerPointToTestAgainst).Terrain().GetTerrainTexture().Colors.Get(myPos, true) * colorStrength;
-		if (res.r.Equals(0) && res.b.Equals(0) && res.g.Equals(0))
-			res.a = 0;
-		return res;
-	}*/
 
 	public static int SeaLevelPixelCount(MapModel Map)
 	{
